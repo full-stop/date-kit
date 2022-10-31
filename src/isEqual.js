f7760981a357127b1997ec100d54594200db1cb3
shenguotao <shenguotao@taptap.com>
2022-10-31 19:09:21 +0800

    feat: modify isEqual method note descriptor.

--- src/isEqual.js
+++ src/isEqual.js
@@ -1,7 +1,12 @@
 import toDate from "./utils/toDate";
 import startOf from "./startOf";
 
-//如果一个日期大于等于另一个日期的起始时间又小于等于其结束时间，那么，这两个日期要么相等，要么具有相等大小的单位。
+/**
+ * 1. 并不关心入参的两个参数大小顺序，因为总有个参数会按照指定的单位得到其起始时间与结束时间。
+ * 2. 如果另一个日期在这个起始时间和结束时间范围区间之内，则说明这个日期如果按照相同的单位转换，两者的区间是相同的，因此两个日期在某个单位下相等，返回值为 true。
+ * 3. (此算法的好处在于无需两个日期都按照固定的单位进行转换，以节省代码量，优化计算)
+ */
+
 function isEqual(startDate, endDate, unit) {
   const date1 = toDate(startDate);
   const date2 = toDate(endDate);
